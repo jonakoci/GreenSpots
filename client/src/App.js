13264ff7412c8b4261ddeb5642a0c91db2aa67d1
jonakoci <127957124+jonakoci@users.noreply.github.com>
2023-06-14 00:53:39 +0200

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 import MapwithMarkers from './MapwithMarkers';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <div style={{margin:"20px", padding:"10px", border:"1px solid #d3d3d3", borderRadius:"10px"}} >
+      <div style={{margin:"20px", padding:"10px"}}>
+       <SearchBar />
+      </div>
+      <div style={{margin:"20px", padding:"10px", border:"1px solid #d3d3d3", borderRadius:"10px"}}>
         <MapwithMarkers/>
-        </div>
+      </div>
+        
       <Footer />
     </div>
   );
